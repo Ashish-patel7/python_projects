d60aacbe423fbdc1db3fc78da2386af0be1152fa
Ashish Kumar Patel <ashishkumar7773333@gmail.com>
2025-09-29 11:45:56 +0530

    adding new file with Python Variables

---
+++ variables.py
@@ -0,0 +1,6 @@
+# A variable is a name given to a memory location in a program.
+# It is used to store and manipulate data in a program.
+
+name = "Ashish" # String variable
+age = 21 # Integer variables
+price = 25.99 #Float variable
