c6a5bfef6aa22f949f737b4cb376c2affba39ebc
Ashish Kumar Patel <ashishkumar7773333@gmail.com>
2025-09-29 22:36:42 +0530

    print output

--- print_sum-1.py
+++ print_sum-1.py
@@ -3,7 +3,7 @@ a = 1000
 b = 500
 sum = a + b
 subtract = a - b
-print(sum)
-print(subtract)
-print("Sum of a and b is:", sum)
-print("Subtract of a and b is:", subtract)
+print(sum) # print: 1500
+print(subtract) # print: 500
+print("Sum of a and b is:", sum) # print: 1500
+print("Subtract of a and b is:", subtract) # print: 500
