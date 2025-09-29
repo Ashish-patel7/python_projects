ce4ee31f961832595bb4cf3d15c2729efb367679
Ashish Kumar Patel <ashishkumar7773333@gmail.com>
2025-09-29 11:09:51 +0530

    writing the code in Python

--- firstProgram.py
+++ firstProgram.py
@@ -1,1 +1,16 @@
 print("Hello World!")
+print("This is my first python program.")
+
+''' Python Character Set
+. letters (a-z, A-Z)
+. digits(0-9)
+. Special Symbols- + - * / etc.
+. Whitespaces- Blank space tab carriage return, newline,formfeed
+. Other characters- Python can process all ASCII nad Unicode characters as part of data or literals
+ '''
+print(99) #Integer
+print(3.24) # Float
+print(4+5j) #Complex Number
+print(True) # boolean
+print(3 + 5) # Arithmatic Expression
+print("hello" + " world") #string concatentation 
