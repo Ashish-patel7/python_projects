61ad4e24d3730947e4ddd54a3893e934f9ab9e25
Ashish Kumar Patel <ashishkumar7773333@gmail.com>
2025-09-29 19:26:42 +0530

    variable type

---
+++ variable_type.py
@@ -0,0 +1,13 @@
+name = "Ashish Kunmar patel"
+age = 21
+price = 45.67
+is_student = True
+courses = ["Math", "science", "History"]
+address = {"street": "123 Main St", "city": "Anytown", "zip": "12345"}
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(is_student))
+print(type(courses))
+print(type(address))
