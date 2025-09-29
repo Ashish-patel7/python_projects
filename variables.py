aabf783429ef2cfa26910b1900c78d1e13a75543
Ashish Kumar Patel <ashishkumar7773333@gmail.com>
2025-09-29 19:04:47 +0530

    Variable naming rules

--- variables.py
+++ variables.py
@@ -4,3 +4,21 @@
 name = "Ashish" # String variable
 age = 21 # Integer variables
 price = 25.99 #Float variable
+age1 = age
+print(name)
+print(age)
+print(price)
+print("my name is:", name)
+print(age1)
+
+# Variable naming rules or rules for Identifiers
+# 1.Identifiers can be  combination of uppercse and lowercase letters,digits or an underscore(_).
+# so myVariable, variable_1, variable_for_print all are valid python identifiers.
+# 2.An Identifier can not start with digit. So while variable1 is valid, 1variable is not valid.
+# 3. We can't use specialsymbols like !, #, @, %, $, etc in our identifier.
+# 4.Identifier can be of any length.
+# 5. Identifiers are cse-sensitive in python. so myVariable and myvariable are two different identifiers.
+# 6. We can not use python keyword as an identifier. Keywords are reserved words in python that have special meaning.
+# Some exmples of keywords are: if , else, while, for, def, return, import,as, in, is, not, and, or, etc.
+
+
