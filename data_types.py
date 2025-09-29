7c0e463058d74b15467d14c8e00a7c7dcde2830a
Ashish Kumar Patel <ashishkumar7773333@gmail.com>
2025-09-29 19:38:30 +0530

    data types

---
+++ data_types.py
@@ -0,0 +1,16 @@
+""""
+* Data Types in Python
+  1. Integer
+  2. String
+  3. Float
+  4. Boolean
+  5. None
+
+"""
+age = 21
+old = False
+a = None
+
+print(type(age))
+print(type(old))  
+print(type(a))      
