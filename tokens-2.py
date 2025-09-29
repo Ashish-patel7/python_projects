0d3338e2eca46d66b291ee9823a7639125776624
Ashish Kumar Patel <ashishkumar7773333@gmail.com>
2025-09-29 20:13:51 +0530

    tokens in python

---
+++ tokens-2.py
@@ -0,0 +1,12 @@
+# types of tokens in python
+  # punctuators are symbols to orginize sentence structure in programming
+
+'''
+ python is Typed language
+ typed are the two types
+ 1. implicit (python is a implicit typed language)
+ 2. explicit (c, c++, java are explicit typed language)
+
+'''
+# (),{},@,[],# etc.
+# -=, +=, *=, /=, //=, **=, %=,= etc.
