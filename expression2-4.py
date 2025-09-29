d52a9d31729609e1af3976adb475ab544e0256b9
Ashish Kumar Patel <ashishkumar7773333@gmail.com>
2025-09-29 22:21:45 +0530

    Expression2 Execution

---
+++ expression2-4.py
@@ -0,0 +1,17 @@
+# Remainder is negative when denominator is negative
+
+A, B = -5, 2
+C = A % B
+print(C)
+
+A, B = 5, 2
+C = A % B
+print(C)
+
+A, B = 5, -2
+C = A % B
+print(C)
+
+A, B = -5, -2
+C = A % B
+print(C)
