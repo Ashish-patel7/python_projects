ca3e473b2579ab80a41ec97cf8a86f35dba6c8bc
Ashish Kumar Patel <ashishkumar7773333@gmail.com>
2025-09-30 09:54:59 +0530

    comment in python

---
+++ comments-5.py
@@ -0,0 +1,13 @@
+# comments in Python
+
+# single line comment 
+
+""" This is 
+    a multi-line comment
+    which spans multiple lines
+    and is enclosed in triple quotes.
+"""
+# Example
+  
+#print("hello") -----> this line is commented out and won't execute
+print("world")  # This will print "world"
